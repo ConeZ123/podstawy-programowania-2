5b82264917f7a05469b3d483b898cfdb7eedb925
ConeZ123 <nismat2004@gmail.com>
2025-03-04 09:28:17 +0100

    update zad1.py

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -1,13 +1,22 @@
 # Algorytm znajdujący największy element w liście
 
-liczby: list = [3, 5, 2, 8, 1]
+liczby: list = [3, -5, 2, 8, 1]
 
 def sort(lista: list):
-    max = liczby[0]
-    for i in lista:
-        if i > max:
-            max = i
-    return max
+    try:
+        max = liczby[0]
+        for i in lista:
+            if i > max:
+                max = i
+        return max
+    except IndexError:
+        return False
+    except TypeError:
+        return False
 
 print(sort(liczby))
                     
+
+def findMax2(liczby: list[int]) -> int:
+    if not liczby:
+        return None
