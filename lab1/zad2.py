feb796e1a44a053ed51070e3c3d757e85496fec6
ConeZ123 <nismat2004@gmail.com>
2025-03-10 22:06:16 +0100

    Add Euklides Modulo (Iteration and Recursion)

--- lab1/zad2.py
+++ lab1/zad2.py
@@ -23,3 +23,32 @@ def eukR(a: int, b: int):
 print("NWD rekurencyjnie:", eukR(8, 26))
 
 # wersja modulo (iteracyjnie i rekurencyjnie)
+
+test_values = [
+        (315, 504),
+        (1230, 528),
+        (28, 8),
+        (12, 18),
+        (10000, 1)
+    ]
+
+def eukModIter(a, b):
+    while b != 0:
+        c = a % b 
+        a = b
+        b = c
+    return a
+
+def eukModRec(a, b):
+    if b == 0:
+        return a
+    else:
+        return eukModRec(b, a % b)
+
+print("Iteracja: ")
+for a, b in test_values:
+    print(f"NWD({a}, {b}) = {eukModIter(a, b)}")
+
+print("\nRekurenja:")
+for a, b in test_values:
+    print(f"NWD({a}, {b}) = {eukModRec(a, b)}")
