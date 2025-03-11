10aa5173f0d889091242aadff734df7b2491685c
ConeZ123 <nismat2004@gmail.com>
2025-03-11 10:21:01 +0100

    Update zad1.py

--- lab2/zad1.py
+++ lab2/zad1.py
@@ -1,5 +1,7 @@
-# znajdowaie dzielnikow i sprawdzanie perwszosci liczby
+import timeit
+import math
 
+# znajdowaie dzielnikow i sprawdzanie perwszosci liczby
 # znajdowanie dzielnikow, ktore dziela sie przez n bez reszty
 
 def znajdzDzielnik(n: int)->list[int]:
@@ -10,3 +12,23 @@ def znajdzDzielnik(n: int)->list[int]:
     return dzielniki
         
 print(znajdzDzielnik(5))
+
+def znajdzDzielnik2(n: int) -> list[int]:
+    return [i for i in range(1, n+1) if n % i == 0]
+
+def znajdzDzielnik3(n: int) -> list[int]:
+    return [i for i in range(1, n // 2+1) if n % i == 0]
+
+n = 10 ** 6
+print("1: ", timeit.timeit(lambda: znajdzDzielnik(n), number=1), "sekundy")
+print("2: ", timeit.timeit(lambda: znajdzDzielnik2(n), number=1), "sekundy")
+print("3: ", timeit.timeit(lambda: znajdzDzielnik3(n), number=1), "sekundy")
+print("4: ", timeit.timeit(lambda: znajdzDzielnik4(n), number=1), "sekundy")
+
+def znajdzDzielnik4(n: int) -> list[int]:
+    dzielniki = []
+    for i in range(1, int(math.sqrt(n))+1):
+        if n % i == 0:
+            dzielniki.append(i)
+            dzielniki.append(n // i)
+    return dzielniki
