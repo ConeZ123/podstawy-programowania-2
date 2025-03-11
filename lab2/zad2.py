c0dda49e622c2f72944132d9aff76dc848dda00f
ConeZ123 <nismat2004@gmail.com>
2025-03-11 10:38:17 +0100

    Update zad2.py

--- lab2/zad2.py
+++ lab2/zad2.py
@@ -1,2 +1,13 @@
 # liczby pierwsze
 # n > 2
+
+def prime(n: int):
+    if n < 2:
+        return 0
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+        else:
+            return True
+        
+print(prime(7))
